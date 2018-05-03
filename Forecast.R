4906291b058b4fd55c1eb3e7e73867f098d9ce0d
SergeyDan <37797215+SergeyDan@users.noreply.github.com>
2018-05-03 15:19:48 +0300

    Update Forecast.R

--- Forecast.R
+++ Forecast.R
@@ -1,5 +1,16 @@
 library(dplyr)
 library(lubridate)
+
+#Чтение данных, правка типа полей
+Prep.Sales.Data = function(SalesCSV){
+  SalesP <- read.csv("SalesP.csv")
+  salesP_df <- tbl_df(SalesP)
+  salesP_df$SKU<-chr(salesP_df$SKU)
+  salesP_df$DEPT<-chr(salesP_df$DEPT)
+  salesP_df$DATE<-as.Date(salesP_df$DATE)
+  salesP_df
+}
+
 #Агрегация по неделям продаж товара 'ItemName'  на АЗС 'DPtName'
 #names(SalesTable)     "DEPT" "SKU"  "DATE" "QNT" 
 WeekDataSales = function(SalesTable,ItemName,DptName) {
