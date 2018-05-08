76d11b8a54963d9e6c1822bcf59b70c58c7515c9
SergeyDan <37797215+SergeyDan@users.noreply.github.com>
2018-05-08 08:27:43 +0300

    Update Forecast.R

--- Forecast.R
+++ Forecast.R
@@ -11,15 +11,25 @@ Prep.Sales.Data = function(SalesCSV){
   salesP_df
 }
 
+#Для анализа по неделям придется убирать 53ю неделю, так как 1 января - всегда первая неделя
 #Агрегация по неделям продаж товара 'ItemName'  на АЗС 'DPtName'
 #names(SalesTable)     "DEPT" "SKU"  "DATE" "QNT" 
 WeekDataSales = function(SalesTable,ItemName,DptName) {
   a<-SalesTable %>%
+    mutate(wday=wday(SalesTable$DATE),month=month(SalesTable$DATE))
+  a<-a %>%
     mutate(year=year(SalesTable$DATE),week=week(SalesTable$DATE)) %>%
     group_by(DEPT,SKU,year,week) %>%
     summarise(WeekQnt=sum(QNT)) %>%
     filter(DEPT==DptName,SKU==ItemName) %>%
     mutate(WeekInd='1') 
+  a$WeekInd<-c(1:length(a$WeekInd))
+  a<-a[a$WeekInd !=1,]
+  a[a$week==52 & a$year==2015,"WeekQnt"]<-a[a$week==52 & a$year==2015,"WeekQnt"] + ceiling(a[a$week==53 & a$year==2015,"WeekQnt"]/2)
+  a[a$week==1 & a$year==2016,"WeekQnt"]<-a[a$week==1 & a$year==2016,"WeekQnt"] + floor(a[a$week==53 & a$year==2015,"WeekQnt"]/2)
+  a[a$week==52 & a$year==2016,"WeekQnt"]<-a[a$week==52 & a$year==2016,"WeekQnt"] + ceiling(a[a$week==53 & a$year==2016,"WeekQnt"]/2)
+  a[a$week==1 & a$year==2017,"WeekQnt"]<-a[a$week==1 & a$year==2017,"WeekQnt"] + floor(a[a$week==53 & a$year==2016,"WeekQnt"]/2)
+  a<-a[a$week !=53,] 
   a$WeekInd<-c(1:length(a$WeekInd))
   a
 }
