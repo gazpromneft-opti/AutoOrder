7b9fb206e9d06520ea96c20dddb443de1e84682f
SergeyDan <37797215+SergeyDan@users.noreply.github.com>
2018-06-05 17:44:31 +0300

    Update PrepData.R
    Добавил:
     Prep.weather.Data - данные погоды - среднесуточные 
    create.promo.table - Подготовка данных об акциях
    vybros.corr - Исправление выбросов
    vybros.values - dataframe выбросов

--- PrepData.R
+++ PrepData.R
@@ -1,5 +1,33 @@
 library(dplyr)
 library(lubridate)
+library(lubridate)
+library(readxl)
+library(smooth)
+
+#данные погоды - среднесуточные с first по final
+Prep.weather.Data = function(CSV,firstd,finald){
+  weather <- read.csv(CSV,sep=";",skip=6,row.names = NULL)
+  a <- tbl_df(weather)[,0:2]; names(a)<-c('DATE','T')
+  a$DATE<- substr(a$DATE, 1, 10)
+  a$DATE<-as.Date(a$DATE, '%d.%m.%Y')
+  b <- a %>%
+    group_by(DATE) %>%
+    summarise(T=mean(T)) %>%
+    filter(DATE>=firstd & DATE<=finald)
+  b
+}
+
+#Подготовка данных об акциях
+create.promo.table = function(xlsx_data,ID_DEP,ID_ITM) {
+  Promo.table <- read_excel(xlsx_data, 13, col_types = c("text", "text", "text", "date", 'date','text','text'), skip = 1)
+  a<-tbl_df(Promo.table[,4:7])
+  a$START<-as.Date(a$START)
+  a$FINISH<-as.Date(a$FINISH)
+  ID_DEP<-'24438'; ID_ITM<-'9900000018616'
+  a<-a %>% 
+    filter(ID_DEPARTMENT==ID_DEP,ID_ITEM==ID_ITM)
+  a 
+}
 
 #Чтение данных, правка типа полей
 Prep.Sales.Data = function(SalesCSV){
@@ -12,6 +40,25 @@ Prep.Sales.Data = function(SalesCSV){
 }
 
 #Исправление выбросов
+vybros.corr = function(data){
+  x<-data$DATE
+  y<-data$QNT
+  # Заберём индексы точек выбросов
+  ind <- which(y %in% boxplot.stats(y)$out)
+  # Сохраним координаты точек выбросов в отдельном dataframe
+  vybrosy <- data.frame(x=x[ind], y=y[ind])
+  ymaxim<-max(y)
+  q=5
+  for (i in ind)
+  { if (i<(q+1)) y[i]<-mean(y[(i+1):(i+1+2*q)])
+    else if (i>(length(x)-q-1)) y[i]<-mean(y[(i-2*q-1):(i-1)])
+    else y[i]<-mean(c(y[(i-q-1):(i-1)],y[(i+1):(i+q+1)]))
+  }
+  y->data$QNT
+  data
+}
+
+#Исправление выбросов, график
 vybros = function(data){
   x<-data$DATE
   y<-data$QNT
@@ -20,8 +67,11 @@ vybros = function(data){
   # Сохраним координаты точек выбросов в отдельном dataframe
   vybrosy <- data.frame(x=x[ind], y=y[ind])
   ymaxim<-max(y)
+  q=5
   for (i in ind)
-  { y[i]<-mean(y[(i-5):(i+5)])
+  { if (i<(q+1)) y[i]<-mean(y[(i+1):(i+1+2*q)])
+  else if (i>(length(x)-q-1)) y[i]<-mean(y[(i-2*q-1):(i-1)])
+  else y[i]<-mean(c(y[(i-q-1):(i-1)],y[(i+1):(i+q+1)]))
   }
   plot(x,y,col='blue', pch=16, ylim=c(0,ymaxim),type='l')
   points(vybrosy$x, vybrosy$y, pch=16, col='red')
@@ -29,6 +79,15 @@ vybros = function(data){
   data
 }
 
+#dataframe выбросов
+vybros.values = function(data){
+  x<-data$DATE
+  y<-data$QNT
+  # Заберём индексы точек выбросов
+  ind <- which(y %in% boxplot.stats(y)$out)
+  # Сохраним координаты точек выбросов в отдельном dataframe
+  data.frame(xv=ind, yv=y[ind])
+}
 
 #Фильтр по дням продаж товара 'ItemName'  на АЗС 'DPtName'
 #names(SalesTable)     "DEPT" "SKU"  "DATE" "QNT" 
